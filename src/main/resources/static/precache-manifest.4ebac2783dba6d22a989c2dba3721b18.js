2ee16d8030402f5f6c9bd751840343ece63aa621
ASUS <ASUS@LAPTOP-N9OVN3H4>
2019-11-05 13:05:11 +0800

    demo

---
+++ src/main/resources/static/precache-manifest.4ebac2783dba6d22a989c2dba3721b18.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2f0c661e286e391a2870f676db17c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6984c5d3bd8823e32ae",
+    "url": "./static/css/2.b8400bb9.chunk.css"
+  },
+  {
+    "revision": "cd971dcaa77b13cce15c",
+    "url": "./static/css/main.a7c80968.chunk.css"
+  },
+  {
+    "revision": "e6984c5d3bd8823e32ae",
+    "url": "./static/js/2.c7669825.chunk.js"
+  },
+  {
+    "revision": "cd971dcaa77b13cce15c",
+    "url": "./static/js/main.29b1d4f5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "92b78071453991060949495003489dd1",
+    "url": "./static/media/12.92b78071.jpg"
+  },
+  {
+    "revision": "b6819e0b23e47fe1e060ec4877a2779a",
+    "url": "./static/media/14.b6819e0b.jpg"
+  },
+  {
+    "revision": "c32809696c1a93d1828f8ebccd595c8d",
+    "url": "./static/media/2.c3280969.jpg"
+  },
+  {
+    "revision": "bcdc3060281bdd8ac2781df1da64d9b9",
+    "url": "./static/media/5.bcdc3060.jpg"
+  }
+]);
